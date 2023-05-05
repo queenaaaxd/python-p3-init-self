f6f6ec310dac7603e370cb8fcd774bfd81f0c3ef
+ <+>
2023-05-05 13:36:53 -0400

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Dog:
-    pass
+    # Remember that all methods use "self".
+    def __init__(self, name, breed = "Mutt"):
+        # print("A dog is born!")
+        # print(f"{name} is born!")
+        self.name = name
+        self.breed = breed
+
+    # def bark(self):
+    #     print("Woof!")
+
+    # def showing_self(self):
+    #     return self
+
+    # ipdb> fido = Dog("Fido")
+    # Fido is born!
+    # ipdb> fido.showing_self()
+    # <__main__.Dog object at 0x1058a3640>
+    # ipdb> fido
+    # <__main__.Dog object at 0x1058a3640>
+    # fido is fido.showing_self() # true
